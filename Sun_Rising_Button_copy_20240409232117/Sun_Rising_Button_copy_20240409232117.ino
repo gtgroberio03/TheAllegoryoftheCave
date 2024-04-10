d25663ad9835b1534ec34cbce65c505125915b59
gtgroberio03 <166449580+gtgroberio03@users.noreply.github.com>
2024-04-09 23:25:29 -0500

    Add files via upload

---
+++ Sun_Rising_Button_copy_20240409232117/Sun_Rising_Button_copy_20240409232117.ino
@@ -0,0 +1,23 @@
+const int buttonPin = 2; 
+int buttonState = 0;
+int previousButtonState = 0;
+
+void setup() {
+  pinMode(buttonPin, INPUT); 
+  Serial.begin(9600); 
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  buttonState = digitalRead(buttonPin); 
+
+  if (buttonState != previousButtonState) {
+    if (buttonState == HIGH){
+      Serial.println("THE SUN RISES"); 
+    } else { 
+      Serial.println(" THE SUN FALLS");
+    }
+  }
+  previousButtonState = buttonState; 
+}
